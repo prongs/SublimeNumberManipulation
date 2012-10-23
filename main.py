f98b2eb873a21b6aca2d7ea82cf098f93defa2a3
Rajat Khandelwal <rajatgupta59@gmail.com>
2012-10-23 23:50:10 +0530

    select next number done

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 import sublime, sublime_plugin
-
-class ExpandSelectionToNumber(sublime_plugin.TextCommand):
+import re
+import itertools
+class SelectNextNumberCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+		nums=list(str(i) for i in xrange(10))
+		sels=self.view.sel()
+		new_sels=[]
+		for sel in sels:
+			found=False
+			start_ind,end_ind=0,0
+			for i in itertools.count(sel.begin()):				
+				s=self.view.substr(i)
+				print s
+				if s==u'\x00':
+					break
+				elif s in nums:
+					found=True
+					start_ind=i
+					break
+			for i in itertools.count(start_ind):
+				if self.view.substr(i) not in nums:
+					end_ind=i;
+					break
+			new_sels.append(sublime.Region(start_ind,end_ind))
+		sels.clear()
+		for sel in new_sels:
+			sels.add(sel)
+
